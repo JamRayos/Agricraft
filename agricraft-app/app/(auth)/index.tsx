c0cff4dc7875f65230e3c3fda938e24267b5a707
BugaXD <jam.rayos091205@gmail.com>
2025-10-26 22:56:40 +0800

    commit

---
+++ agricraft-app/app/(auth)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import {Redirect} from "expo-router";
+import {useEffect} from "react";
+import { router } from "expo-router";
+
+export default function Index() {
+    return <Redirect href="/(auth)/sign-up" />;
+}
+
+// the homescreen
